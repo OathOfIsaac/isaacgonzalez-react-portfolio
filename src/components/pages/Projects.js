806f2c8e2a40dd158ee1fb26ca19ebd9a57e60a8
Oath Of Isaac <isaacfgnzlz@gmail.com>
2022-06-28 17:44:50 -0500

    added navbar with pages for About , Contact , Projects, screenshots and cover image added to assets, Navtabs.js, PortfolioContainer.js, no errors

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import sikester1 from '../../assets/large/projects/0-sikester-main.png';
+import sikester2 from '../../assets/large/projects/0-sikester.png'
+import fetch1 from '../../assets/large/projects/1-fetch.png'
+import fetch2 from '../../assets/large/projects/1-fetch-2.png'
+
+
+
+export default function Projects() {
+  
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <img src={sikester1} alt="sikester homepage"></img>
+      <p>
+        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
+        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
+        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
+        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
+        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
+        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
+        libero. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
+        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
+        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
+        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
+        est ligula suscipit velit, sed bibendum turpis dui in sapien.
+      </p>
+    </div>
+  );
+}
